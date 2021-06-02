65e96d38b34c064edfd03fe7c9cf87f68e672f38
MaiDTTb <maidttb@bkav.com>
2021-06-02 11:02:55 +0700

    'test'

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 function App() {
   return (
+      <>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -18,7 +19,14 @@ function App() {
           Learn React
         </a>
       </header>
+
     </div>
+          <MessengerCustomerChat
+              pageId="106941058270737"
+              appId="168962168507958"
+          />
+          <div>hello</div>
+          </>
   );
 }
 
